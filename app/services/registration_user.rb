6214dbfb2813dd06075850f3c92ac11c5fbdfd61
Michael <ekert.michael@gmail.com>
2022-09-25 17:23:04 +0200

    Added better validation for registration new users

--- app/services/registration_user.rb
+++ app/services/registration_user.rb
@@ -1,4 +1,5 @@
 class RegistrationUser  < ApplicationService
+
   def initialize(email,password)
     @email = email
     @password = password
@@ -8,20 +9,25 @@ class RegistrationUser  < ApplicationService
   attr_accessor :email, :password
 
   def call
-    if uniq_credential == nil
-    User.create(
-      email: email,
-      password: password
-    )
+    if uniq_credential != nil
+      errors.add(:registration_error, "Email probably is already taken")
+    elsif email.blank? || password.blank? || valid_email == false
+      errors.add(:invalid_credentials, "Ivalid credentials")
     else
-    errors.add(:registration_error, 'Invalid email or passsword')
+      User.create(
+        email: email,
+        password: password
+      )
     end
-    end
+  end
 
   private
+
+  def valid_email
+    URI::MailTo::EMAIL_REGEXP.match?(email)
+  end
 
   def uniq_credential
     @uniq_credential = User.find_by(email: email)
   end
-
 end
